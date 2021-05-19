8424fe30941b1c6c732a4617db288d5ceb90bcc8
thaisc98 <thaisc98@hotmail.es>
2021-05-18 23:36:49 -0400

    [new] reciving from console and checkvalues

--- DivisionGroups/Program.cs
+++ DivisionGroups/Program.cs
@@ -4,9 +4,45 @@ namespace DivisionGroups
 {
     class Program
     {
+
+        class Divition
+        {
+
+            public void checkValues(int countGroups, string pathStudents, string pathTopics)
+            {
+                if (countGroups <= 0)
+                {
+                    throw new Exception("La cantidad grupos es menor o igual que 0.");
+                }
+
+                if (pathStudents == null || pathTopics == null)
+                {
+                    throw new Exception("Las direcciones ingresadas no  son validas.");
+                }
+
+                Console.WriteLine("All is correct");
+
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Divition divition = new Divition();
+
+            Console.WriteLine("Cantidad de grupos: ");
+            int countGroups = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("La direccion de los estudiantes: ");
+            string pathStudents = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("La direccion de los temas: ");
+            string pathTopics = Convert.ToString(Console.ReadLine());
+
+            divition.checkValues(countGroups, pathStudents, pathTopics);
+
         }
     }
 }
