b7c701800430f30d059c50a1ae94fdadf01ee641
thaisc98 <thaisc98@hotmail.es>
2021-05-18 23:28:35 -0400

    [new] first commit

---
+++ DivisionGroups/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DivisionGroups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
