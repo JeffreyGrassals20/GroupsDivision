670f48854604ea902d4884249f4ee172161f6ba4
Yamilka <Yamilka_268465460@hotmail.com>
2021-05-19 00:52:48 -0400

    Version with divition students-Groups-Topics

--- DivisionGroups/Program.cs
+++ DivisionGroups/Program.cs
@@ -11,7 +11,11 @@ namespace DivisionGroups
 
             public List<string> students = new List<string>();
             public List<string> topics = new List<string>();
-            
+            public List<string> Selection = new List<string>();
+            public List<string> Exeptions = new List<string>();
+            public List<string> Groups = new List<string>();
+
+
             public void checkValues(int countGroups, string pathStudents, string pathTopics)
             {
                 if (countGroups <= 0)
@@ -34,27 +38,115 @@ namespace DivisionGroups
 
                 System.IO.StreamReader fileStudents = new System.IO.StreamReader(pathStudents);
 
-                while((line = fileStudents.ReadLine()) != null){
+                while ((line = fileStudents.ReadLine()) != null)
+                {
                     students.Add(line);
-                } 
+                }
 
                 line = null;
                 System.IO.StreamReader fileTopics = new System.IO.StreamReader(pathTopics);
 
-                while((line = fileStudents.ReadLine()) != null){
+                while ((line = fileStudents.ReadLine()) != null)
+                {
                     topics.Add(line);
-                }   
+                }
 
-                if (countGroups > students.Count){
+                if (countGroups > students.Count)
+                {
                     Console.Clear();
                     throw new Exception("La cantidad de grupos no debe ser mayor que la de estudiantes");
                 }
 
             }
 
+            public void Divition_Students_Topics_Grups(int quantity)
+            {
 
+                var random = new Random();
+                int index;
+                int index2;
+                int index3;
+                int counter = 0;
+
+                while (topics.Count > 0)
+                {
+
+
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        Groups.Add("GRUPO#" + i + 1);
+                    }
+
+                    while (Groups.Count > 0 && topics.Count > 0)
+                    {
+
+                        index = random.Next(Groups.Count);
+                        index2 = random.Next(topics.Count);
+                        index3 = random.Next(students.Count);
+                    }
+
+                    //PRIMERA CORRIDA
+
+                    if (counter == 0)
+                    {
+                        Selection.Add(Groups[index] + "   TEMAS: ->" + topics[index2]);
+                        Groups.RemoveAt(index);
+                        topics.RemoveAt(index2);
+                    }
+                    //DEMAS CORRIDAS
+                    else
+                    {
+                        for (int i = 0; i < Selection.Count; i++)
+                        {
+                            if (Selection[i].IndexOf(Groups[index]) != -1)
+                            {
+
+                                Selection[i] = (Selection[i] + "  ," + topics[index2]);
+                                Groups.RemoveAt(index);
+                                topics.RemoveAt(index2);
+                                break;
+                            }
+                        }
+                    }
+                    counter++;
+                }
+
+
+                for (int i = 0; i < Selection.Count; i++)
+                {
+                    Groups.Add("ESTUDIANTES: ->" + i + 1);
+                }
+
+
+                while (students.Count > 0)
+                {
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        Groups.Add("GRUPO#" + i + 1);
+                    }
+
+                    while (students.Count > 0 && Groups.Count() > 0)
+                    {
+
+                        index = random.Next(Groups.Count);
+                        index3 = random.Next(students.Count);
+
+                        for (int i = 0; i < Selection.Count; i++)
+                        {
+                            if (Selection[i].IndexOf(Groups[index]) != -1)
+                            {
+
+                                Selection[i] = (Selection[i] + "  ," + students[index3]);
+                                Groups.RemoveAt(index);
+                                students.RemoveAt(index3);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
         }
-
         static void Main(string[] args)
         {
 
@@ -72,6 +164,7 @@ namespace DivisionGroups
             divition.checkValues(countGroups, pathStudents, pathTopics);
             divition.readFiles(countGroups, pathStudents, pathTopics);
 
+            Console.ReadKey();
         }
     }
 }
